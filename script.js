68bdfc79ceb0fe32264d97f139c426bed38fb845
ParanormalSa <o19salehi@gmail.com>
2025-01-01 15:51:43 +0330

    Create script.js

---
+++ script.js
@@ -0,0 +1,9 @@
+// برای باز کردن اسلاید بار
+function openSlidebar() {
+    document.getElementById("slidebar").classList.add("open");
+}
+
+// برای بستن اسلاید بار
+function closeSlidebar() {
+    document.getElementById("slidebar").classList.remove("open");
+}
